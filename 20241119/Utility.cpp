69f05eb30da4c485467d536f7eaed6aae34a29df
4b3g0043@office.stust.edu.tw <4b3g0043@office.stust.edu.tw>
2024-12-09 20:49:10 +0800

    新增初始化函數及教師課程分配功能
    在 `2024CourseApp.cpp` 中新增 `InitializeData` 函數並在 `main` 中呼叫，並將 `Student` 和 `Teacher` 的部分屬性改為中文，新增多個 `Course` 物件並分配給 `Teacher`。更新 `2024CourseApp.vcxproj` 和 `2024CourseApp.vcxproj.filters` 以引用 `2024CourseApp.h`。在 `Teacher.cpp` 和 `Teacher.h` 中新增 getter、setter 和 `display` 方法，並將 `courses` 變數重新命名為 `teachingCourses`。更新 `README.md`，新增資訊封裝和 vector 容器的連結。新增 `2024CourseApp.h`，包含 `#pragma once` 和 `InitializeData` 函數的宣告。

---
+++ 20241119/Utility.cpp
@@ -0,0 +1,42 @@
+﻿#include "Utility.h"
+#include <string>
+
+string Utility::toString(Department department)
+{
+	switch (department) {
+	case Department::ComputerSciece:
+		return "��T�u�{";
+	case Department::ElectricalEngineering:
+		return "�q���u�{";
+	case Department::ElectronicEngineering:
+		return "�q�l�u�{";
+	case Department::InformationManagement:
+		return "��T�޲z";
+	default:
+		return "Unknown Department";
+	}
+}
+
+string Utility::toString(ClassName className)
+{
+	switch (className) {
+	case ClassName::_1A:
+		return "�@��";
+	case ClassName::_1B:
+		return "�@�A";
+	case ClassName::_2A:
+		return "�G��";
+	case ClassName::_2B:
+		return "�G�A";
+	case ClassName::_3A:
+		return "�T��";
+	case ClassName::_3B:
+		return "�T�A";
+	case ClassName::_4A:
+		return "�|��";
+	case ClassName::_4B:
+		return "�|�A";
+	default:
+		return "����";
+	}
+}
